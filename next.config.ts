1b697010fdab67c86fa2fb309d96ab362e9a157d
Josué Barra Zavala <jo.barrazav@gmail.com>
2025-07-17 05:39:18 -0400

    feat: add redirects from root to feed

--- next.config.ts
+++ next.config.ts
@@ -14,6 +14,15 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/feed",
+        permanent: true,
+      },
+    ];
+  },
   async headers() {
     return [
       {
